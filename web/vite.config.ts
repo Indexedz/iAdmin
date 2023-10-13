9853e5b4ee7253e13fac0011daaed6ee21bc43bc
iwind <134185860+iwindd@users.noreply.github.com>
2023-10-13 08:24:58 +0700

    vite build not show

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './',
+  publicDir: 'public',
+  build: {
+    target: 'esnext',
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+});
