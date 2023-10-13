f803495b914c7663fb806da6bd034d490426ce39
iwind <134185860+iwindd@users.noreply.github.com>
2023-10-13 08:14:42 +0700

    close hud

--- modules/nui/index.lua
+++ modules/nui/index.lua
@@ -1,6 +1,5 @@
 local module = {}
-
-RegisterNuiCallback("hide", module.hide)
+local visiblity = false
 
 function module.send(header, props)
   SendNUIMessage({
@@ -27,12 +26,26 @@ end
 
 function module.show()
   SetNuiFocus(true, true)
-  
+  SetNuiFocusKeepInput(true);
+  visiblity = true;
+  CreateThread(function ()
+    while visiblity do
+      Wait(1)
+
+      if IsDisabledControlJustPressed(0, 25) then
+        break;
+      end
+    end
+
+    module.hide()
+  end)
+
   return module.send("setNavigate", "primary")
 end
 
 function module.hide()
   SetNuiFocus(false, false)
+  visiblity = false;
 
   return module.send("setNavigate", "hidden")
 end
